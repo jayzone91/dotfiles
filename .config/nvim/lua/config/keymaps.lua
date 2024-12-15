23c1739d152b768dfbf7584cda359e63d20c086f
jayzone91 <register@j4yz0n3.de>
2024-12-15 09:05:22 +0100

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,27 @@
 -- Keymaps for standard Neovim Functions
 
 vim.keymap.set("n", "<leader>qq", "<cmd>qa<CR>", { desc = "Quit Neovim" })
+
+-- Terminal
+
+vim.api.nvim_create_autocmd("TermOpen", {
+  group = vim.api.nvim_create_augroup("custom-terminal", { clear = true }),
+  callback = function()
+    vim.opt.number = false -- deactivate line numbers in terminal
+    vim.opt.relativenumber = false -- deactivate relative line numbers in terminal
+  end,
+})
+
+vim.keymap.set("n", "<leader>sf", function()
+  vim.cmd.vnew()
+  vim.cmd.term()
+  vim.cmd.wincmd("J")
+  vim.api.nvim_win_set_height(0, 15)
+end, { desc = "Small Terminal Split" })
+
+vim.keymap.set(
+  "t",
+  "<esc>",
+  "<C-\\><C-N>",
+  { desc = "Easy Leave Terminal Mode" }
+)
