63b36bd238fbb489641ab52f5d03933d66190937
jayzone91 <register@j4yz0n3.de>
2025-03-25 12:18:11 +0100

    Update snacks.lua

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -48,6 +48,11 @@ return {
   "folke/snacks.nvim",
   dependencies = {
     {
+      "folke/flash.nvim",
+      event = "VeryLazy",
+      opts = {},
+    },
+    {
       "s1n7ax/nvim-window-picker",
       name = "window-picker",
       opts = {
@@ -174,6 +179,25 @@ return {
       sources = {
         explorer = {
           actions = {
+            flash = function(picker)
+              require("flash").jump({
+                pattern = "^",
+                label = { after = { 0, 0 } },
+                search = {
+                  mode = "search",
+                  exclude = {
+                    function(win)
+                      return vim.bo[vim.api.nvim_win_get_buf(win)].filetype
+                        ~= "snacks_picker_list"
+                    end,
+                  },
+                },
+                action = function(match)
+                  local idx = picker.list:row2idx(match.pos[1])
+                  picker.list:_move(idx, true, true)
+                end,
+              })
+            end,
             window_picker = function(_, item)
               if item.dir then
                 return
@@ -191,6 +215,12 @@ return {
             end,
           },
           win = {
+            input = {
+              keys = {
+                ["<a-s>"] = { "flash", mode = { "n", "i" } },
+                ["s"] = { "flash" },
+              },
+            },
             list = {
               keys = {
                 ["w"] = "window_picker",
