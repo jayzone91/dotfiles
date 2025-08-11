750974cf380a3db0f285f12c2396aa919f81fe48
jayzone91 <register@j4yz0n3.de>
2025-08-11 10:29:42 +0200

    Update keymaps.lua

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -5,3 +5,4 @@ end
 map("n", "<leader>qq", ":qa<CR>", "Neovim beenden")
 map("n", "<c-s>", ":w<CR>", "Save Buffer")
 map({ "n", "i" }, "<C-s>", "<Esc>:w<CR>", "Buffer speichern")
+map("n", "H", vim.lsp.buf.hover, "")
