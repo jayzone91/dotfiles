4b4eb600f3293b55b115e58ef8fd212b045c8a4c
Johannes Kirchner <register@j4yz0n3.de>
2026-07-29 19:48:02 +0200

    alles neu

--- .config/nvim/lua/plugins/boundary.nvim.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "Kenzo-Wada/boundary.nvim",
-  branch = "release",
-  event = { "BufReadPre", "BufNewFile" },
-  opts = {
-    auto = true,
-  },
-}
--- .config/nvim/lua/plugins/mason-lspconfig.nvim.lua
+++
@@ -1,5 +0,0 @@
-return {
-  "mason-org/mason-lspconfig.nvim",
-  dependencies = { "mason-org/mason.nvim", "neovim/nvim-lspconfig" },
-  opts = {},
-}
